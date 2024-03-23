e01fb0785ea6ae200b3a2388f3088ad27213490d
Rohit6062 <rohitranware0096@gmail.com>
2024-03-23 09:41:44 +0000

    still having tree with 17

--- hfe.c
+++ hfe.c
@@ -39,7 +39,6 @@ void heapifyNode(node *a, int n, int i){
 }
 
 void heapNode(node *a, int n){
-	printf("In HeapNode function widt %d \n",n);
 	int j = n - 1;
 	for(int i = n/2 - 1; i >=0; i--){
 		heapifyNode(a, n, i);
@@ -60,23 +59,19 @@ void combNode(node* left,node * right,node* tmp){
 
 void traverseTree(node *root)
 {
-	if(root==NULL)	return;
-	
-	if(root->left){
+	if(root!=NULL){
+		printf("own %p left-> %p data-> %d right->%p ascii-> %d\n",root,root->left,root->data,root->right,root->ascii);
 		traverseTree(root->left);
-	}
-	printf("own %p left-> %p data-> %d right->%p ascii-> %d\n",root,root->left,root->data,root->right,root->ascii);
-	if(root->right){
 		traverseTree(root->right);
 	}
-
 }
 
 int main(){
     int map[10][2]={ {97,10},{98,101},{99,11},{100,102},{103,9},{105,10},{107,101},{106,11},{109,102},{111,7}};	
 	int n=sizeof(map)/sizeof(map[0]);
 	printf("%d\n",n);
-	node sta[n];
+	node* sta;
+	sta=malloc(n*sizeof(node));
 	int i=0;
 
 	// this will create node for every element in map;
@@ -105,8 +100,11 @@ int main(){
 			tmp2=malloc(sizeof(node*));
 			*tmp1=sta[k-1];
 			*tmp2=sta[k-2];
+			sta=realloc(sta,k*sizeof(node));
 			combNode(tmp1,tmp2,&sta[k-2]);
 			k--;
+			node* root = &sta[k-2];
+			printf("Main While own %p left-> %p data-> %d right->%p ascii-> %d\n",root,root->left,root->data,root->right,root->ascii);
 		}
 		else{
 			break;

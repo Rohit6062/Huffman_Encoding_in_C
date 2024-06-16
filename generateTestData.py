16840622324edec3feff6ca9b6de7d5434f5044d
Rohit6062 <rohitranaware0096@gmail.com>
2024-06-16 10:50:40 +0530

    updated genrate_data.py

--- generateTestData.py
+++ generateTestData.py
@@ -8,8 +8,8 @@ out=2
 for i in range(2,26):
     li[i]=out
     out+=out
-print(li)
 f= open('Data.txt','w')
+print("Generating...")
 for i in range(26):
     for j in range(li[i]):
         f.write(chr(97+i))
